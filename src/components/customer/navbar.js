5313fa6bc268f2c024fd2dddc10b63c43574d20d
Scythektk <kartikechopra@gmail.com>
2022-04-04 23:30:11 +0530

    merge yashb and kartike

---
+++ src/components/customer/navbar.js
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+
+import './customer.css';
+
+import cart from '../assets/cart.png';
+
+const Navbar = () => {
+    const [anchorElNav, setAnchorElNav] = useState(null);
+    const [anchorElUser, setAnchorElUser] = useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="sticky">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    ></IconButton>
+                    <MenuIcon></MenuIcon>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1 }}
+                    >
+                        News
+                    </Typography>
+                    <Button color="inherit" className="cart-img-container">
+                        <img src={cart} alt="Cart" />
+                    </Button>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+};
+
+export default Navbar;
