2cb25c75f2892b41d7e0ccf94fefe75338117ace
Yash Barge <yhbarge@gmail.com>
2022-04-05 22:53:02 +0530

    removed unnecessary comments and files

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyATCAOXlPaQ77ItDw0D5aNDootlvwJQ_Os",
   authDomain: "fir-tutorial-9f571.firebaseapp.com",
@@ -14,7 +10,6 @@ const firebaseConfig = {
   appId: "1:432183119952:web:8da2627e72300f2a4e4155"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
