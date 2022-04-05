5683fa5432891edade9c9ad6bc92cf9b66b29938
Scythektk <kartikechopra@gmail.com>
2022-04-06 00:22:29 +0530

    add category pages

--- src/components/customer/fruits-veg.js
+++ src/components/customer/fruits-veg.js
@@ -1,20 +1,22 @@
-import { useState, useEffect } from "react";
-import { db } from "../../firebase-config.js";
-import {
-    collection,
-    getDocs,
-    onSnapshot,
-} from "firebase/firestore";
+import { useState, useEffect } from 'react';
+import { db } from '../../firebase-config.js';
+import { collection, getDocs, onSnapshot } from 'firebase/firestore';
+
+import './customer.css';
+
+import fruits from '../../assets/fruits.jpeg';
 
 const FruitsVeg = () => {
     const [items, setItems] = useState([]);
-    const itemsCollectionRef = collection(db, "item");
+    const itemsCollectionRef = collection(db, 'item');
 
     useEffect(() => {
         const getItems = async () => {
             onSnapshot(itemsCollectionRef, async () => {
                 const data = await getDocs(itemsCollectionRef);
-                setItems(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+                setItems(
+                    data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+                );
             });
         };
 
@@ -23,19 +25,21 @@ const FruitsVeg = () => {
 
     return (
         <div>
-            {items.map((item) => {
-                return (
-                    <div>
-                        {" "}
-                        <h1>Name: {item.name}</h1>
-                        <h1>Price: {item.price}</h1>
-                        <h1>Quantity: {item.quantity}</h1>
-                        <h1>Category: {item.category}</h1>
-                    </div>
-                );
-            })}
+            <h3>Fruits and Vegetables</h3>
+            <div className="category-flex-container">
+                {items.map((item) => {
+                    return (
+                        <div>
+                            <img src={fruits} alt="" />
+                            <h3>{item.name}</h3>
+                            <p>Price: {item.price}</p>
+                            <p>Quantity: {item.quantity}</p>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default FruitsVeg;
--- src/components/customer/navbar.js
+++ src/components/customer/navbar.js
@@ -2,14 +2,22 @@ import { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import MenuIcon from '@material-ui/icons/Menu';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
 
 import './customer.css';
 
 import cart from '../../assets/cart.png';
+
+const pages = ['Home', 'Blog', 'Features', 'Pricing', 'Documentation'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const Navbar = () => {
     const [anchorElNav, setAnchorElNav] = useState(null);
@@ -31,32 +39,144 @@ const Navbar = () => {
     };
 
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="sticky">
-                <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    ></IconButton>
-                    <MenuIcon></MenuIcon>
+        <AppBar
+            position="static"
+            className="navbar"
+            elevation={0}
+            sx={{ color: 'black' }}
+        >
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
                     <Typography
                         variant="h6"
+                        noWrap
                         component="div"
-                        sx={{ flexGrow: 1 }}
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            color: 'black',
+                        }}
                     >
-                        News
+                        LOGO
                     </Typography>
-                    <Button color="inherit" className="cart-img-container">
-                        <img src={cart} alt="Cart" />
-                    </Button>
-                    <Button color="inherit">Login</Button>
+
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: 'flex', md: 'none' },
+                        }}
+                    >
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        ></IconButton>
+                        <MenuIcon className="burger-img"></MenuIcon>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem
+                                    key={page}
+                                    onClick={handleCloseNavMenu}
+                                >
+                                    <Typography textAlign="center">
+                                        {page}
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: 'flex', md: 'none' },
+                        }}
+                    >
+                        LOGO
+                    </Typography>
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: 'none', md: 'flex' },
+                        }}
+                    >
+                        {pages.map((page) => (
+                            <Button
+                                className="button-text"
+                                key={page}
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, color: 'black', display: 'block' }}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title="Open settings">
+                            <IconButton
+                                onClick={handleOpenUserMenu}
+                                sx={{ p: 0 }}
+                            >
+                                <Avatar
+                                    alt="Remy Sharp"
+                                    src="/static/images/avatar/1.jpg"
+                                />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem
+                                    key={setting}
+                                    onClick={handleCloseUserMenu}
+                                >
+                                    <Typography textAlign="center">
+                                        {setting}
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
                 </Toolbar>
-            </AppBar>
-        </Box>
+            </Container>
+        </AppBar>
     );
 };
-
 export default Navbar;
